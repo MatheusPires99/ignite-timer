2261c34356303a9de6fc0107baecebd1a9d58748
Matheus Pires <matheushenriquepires99@gmail.com>
2022-07-11 11:28:40 -0300

    chore: Config react-router-dom

--- src/components/Button/index.tsx
+++
@@ -1,9 +0,0 @@
-import { ButtonContainer, ButtonVariant } from './styles'
-
-export type ButtonProps = {
-  variant?: ButtonVariant
-}
-
-export const Button = ({ variant = 'primary' }: ButtonProps) => {
-  return <ButtonContainer variant={variant}>Enviar</ButtonContainer>
-}
